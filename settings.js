ec473728a3e9a7f61a81f8f932904eeb49870cb4
Kincah <150204920+Kincah@users.noreply.github.com>
2023-11-08 18:18:13 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,19 +14,19 @@ require("dotenv").config();
 
 //_______________________ ┏  Main Setting  ┓ _______________________\\
 
-creator = "乂𝙍𝙖𝙯𝙖𝙣乂"; // Nama
+creator = "Kinchan"; // Nama
 port = 8080; //port host
 LimitApikey = 200; // Limit Apikey default
 
 //___________ ┏  Database Mongodb Setting  ┓ ___________\\
 
-keymongodb = process.env.mongodb; //example "mongodb+srv://user:password@name_database.mongodb.net/?retryWrites=true&w=majority";
+keymongodb = process.env.mongodb; //example "mongodb+srv://vihamlihan:LzSN3cf4Y4eqjZDE@cluster0.jg3es5y.mongodb.net/?retryWrites=true&w=majority";
 
 //___________ ┏  SMTP Setting  ┓ ___________\\
 
 //note service SMTP Gmail Tidak Boleh Send Kat Temp Email Kalau mahu pakai temp email buat di service lain
-usetempemail = true; // kalau true boleh sing up pakai email temp kalau pakai service gmail pakai false je
-servicesmtp = "Zoho"; //Ada gmail ada sendinblue ikut masing2 limit gmail 500 limit sendinblue 300 free. List service smtp : https://nodemailer.com/smtp/well-known/
+usetempemail = false; // kalau true boleh sing up pakai email temp kalau pakai service gmail pakai false je
+servicesmtp = "vihamlihan@gmail.com"; //Ada gmail ada sendinblue ikut masing2 limit gmail 500 limit sendinblue 300 free. List service smtp : https://nodemailer.com/smtp/well-known/
 sendemail = process.env.sendemail; //Email SMTP
 fromsendemail = process.env.fromsendemail; // From Email SMTP *kalau takde custom email kena sama email dengan Email SMTP
 sendpwmail = process.env.sendpwmail; // Password Email SMTP
